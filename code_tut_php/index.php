2d555d933e51fbd1e8fb51883643d7e1f7a6217c
unknown <alshaary2016@gmail.com>
2019-04-04 19:13:36 +0200

    first commit

---
+++ code_tut_php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>This is PHP Login With MySQL DB</title> 
+    <link rel="stylesheet" href="css/style.css"/>
+</head>
+
+<body>
+    
+    <div class="form">
+        <h1>Sign Up</h1>
+        <form action="" method="post" >
+            <input type="text" name="name" placeholder="Your Name..."/>
+            <input type="email" name="email" placeholder="Your Email..."/>
+            <input type="password" name="password" placeholder="Your Password..."/>
+        </form>
+    </div>
+</body>
+</html>
